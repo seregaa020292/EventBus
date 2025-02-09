a45c1f7e663723be1f07e40e6c4386c5256d2105
seregaa020292 <40691867+seregaa020292@users.noreply.github.com>
2025-02-09 19:15:28 +0300

    Update middleware.go

--- middleware.go
+++ middleware.go
@@ -4,17 +4,17 @@ import "sync"
 
 type Middleware func(next Handler) Handler
 
-type middleware struct {
-	middlewares []Middleware
+type middlewares struct {
+	chain []Middleware
 }
 
-func (m *middleware) Append(mw ...Middleware) {
-	m.middlewares = append(m.middlewares, mw...)
+func (m *middlewares) Append(mw ...Middleware) {
+	m.chain = append(m.middlewares, mw...)
 }
 
-func (m *middleware) Wrap(h Handler) Handler {
-	for i := len(m.middlewares) - 1; i >= 0; i-- {
-		h = m.middlewares[i](h)
+func (m *middlewares) Wrap(h Handler) Handler {
+	for i := len(m.chain) - 1; i >= 0; i-- {
+		h = m.chain[i](h)
 	}
 	return h
 }
